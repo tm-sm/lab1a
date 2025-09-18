92405c3c93b7c4f1e2803ef7bb48775a8418eb91
tm-sm <84805291+tm-sm@users.noreply.github.com>
2025-09-18 18:10:55 +0200

    first commit

---
+++ semaphores.adb
@@ -0,0 +1,18 @@
+-- Package: Semaphores
+--
+-- ==> Complete the code at the indicated places
+
+package body Semaphores is
+   protected body CountingSemaphore is
+      entry Wait when Count > 0 is -- To be completed
+      begin
+         Count := Count - 1;
+      end Wait;
+
+      entry Signal when Count < Max is -- To be completed
+      begin
+         Count := Count + 1;
+      end Signal;
+   end CountingSemaphore;
+end Semaphores;
+
