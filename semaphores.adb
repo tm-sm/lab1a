91eb1960fda300278b56bdc5326f86a75fc284c5
tm-sm <84805291+tm-sm@users.noreply.github.com>
2025-09-18 18:26:25 +0200

    inverts semaphore

--- semaphores.adb
+++ semaphores.adb
@@ -4,14 +4,14 @@
 
 package body Semaphores is
    protected body CountingSemaphore is
-      entry Wait when Count > 0 is -- To be completed
-      begin
-         Count := Count - 1;
-      end Wait;
-
-      entry Signal when Count < Max is -- To be completed
+      entry Wait when Count < Max is -- To be completed
       begin
          Count := Count + 1;
+      end Wait;
+
+      entry Signal when Count > 0 is -- To be completed
+      begin
+         Count := Count - 1;
       end Signal;
    end CountingSemaphore;
 end Semaphores;
